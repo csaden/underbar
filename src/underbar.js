7febb4488b7976641466571d7deb9cdb30400ff8
Chris Saden <chris.saden@udacity.com>
2015-06-29 17:27:48 -0700

    updated reject to use filter

--- src/underbar.js
+++ src/underbar.js
@@ -99,13 +99,9 @@
   _.reject = function (collection, test) {
     // TIP: see if you can re-use _.filter() here, without simply
     // copying code in and modifying it
-    var res = [];
-    _.filter(collection, function (item) {
-      if (!(test(item))) {
-        res.push(item);
-      }
+    return _.filter(collection, function (item) {
+      return !test(item);
     });
-    return res;
   };
 
   // Produce a duplicate-free version of the array.
